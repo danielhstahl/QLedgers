8540f6b6004cf1305c5b527d9a36cbeefd91e254
Daniel Stahl <danstahl1138@gmail.com>
2017-05-28 11:41:31 -0500

    improved comments in startChain.sh

--- startChain.sh
+++ startChain.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#~/Documents/ExternalApps/quorum/build/bin/bootnode –genkey bootnode.txt 
-#nohup ~/Documents/ExternalApps/quorum/build/bin/geth --datadir "./" init quorum-genesis.json  > gethOutput.log &
+#THIS IS THE FORKED VERSION!  https://github.com/phillyfan1138/quorum-examples
 (cd ~/Documents/ExternalApps/quorum-examples/examples/7nodes;./init.sh $GOPATH/bin;./start.sh $GOPATH/bin)
