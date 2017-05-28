1686e1bf5f4dd1e732e63e2e4bc540638ab403bf
Daniel Stahl <danstahl1138@gmail.com>
2017-05-28 10:05:17 -0500

    improved script

--- startChain.sh
+++ startChain.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #~/Documents/ExternalApps/quorum/build/bin/bootnode –genkey bootnode.txt 
 #nohup ~/Documents/ExternalApps/quorum/build/bin/geth --datadir "./" init quorum-genesis.json  > gethOutput.log &
-~/Documents/ExternalApps/quorum-examples/examples/7nodes/init.sh ~/Documents/ExternalApps/quorum/build/bin
-~/Documents/ExternalApps/quorum-examples/examples/7nodes/start.sh ~/Documents/ExternalApps/quorum/build/bin 
+(cd ~/Documents/ExternalApps/quorum-examples/examples/7nodes;./init.sh ~/Documents/ExternalApps/quorum/build/bin;./start.sh ~/Documents/ExternalApps/quorum/build/bin)
